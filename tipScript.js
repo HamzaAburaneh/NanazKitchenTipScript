0ec39c92ff2912ff6fc4fdc3a79a683a56bf9876
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2020-07-22 14:49:09 -0400

    Update tipScript.js

--- tipScript.js
+++ tipScript.js
@@ -1,47 +1,15 @@
-script<type="application/json">
+// Initialize extra fields
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
 
-/*
- * Show a promo 'Free shipping' message on the cart page. 
- * In the example below, users will see a message on the cart page 
- * informing them that they will qualify for free shipping if their 
- * order is more than $99
- */
+// Add a new optional text input 'How should we sign the package?' to shipping address form
+ec.order.extraFields.wrapping_box_signature = {
+    'title': 'How should we sign the package?',
+    'textPlaceholder': 'Package sign',
+    'type': 'text',
+    'tip': 'We will put a label on a box so the recipient knows who it is from',
+    'required': false,
+    'checkoutDisplaySection': 'shipping_address'
+};
 
-var promoMessage = "Orders $99 and up ship free!";
-var minSubtotal = 99;
-var widgets;
-
-// Calculating subtotal and displaying the message
-var checkSubtotal = function(order) {
-  if (order) {
-    var subtotal = order.total;
-    if (subtotal > minSubtotal) {
-      alert(promoMessage);
-    }  
-  }
-}
-
-// Detecting whether we're on the cart page and get the cart info
-Ecwid.OnPageLoaded.add(function(page) {
-  widgets = Ecwid.getInitializedWidgets();
-
-  // if storefront widget is present on this page
-  for (i = 0; i < widgets.length ; i++) {
-    if (widgets[i] == "ProductBrowser") {
-      if ('CART' == page.type) {
-        alert("test");
-        Ecwid.Cart.calculateTotal(function(order) {
-          checkSubtotal(order);
-        });
-      }
-    }
-  }
-});
-
-// Get color value for the message and store it in color variable
-var color = Ecwid.getAppPublicConfig(appId);
-
-// your code here
-// ...
-
-</script>
+Ecwid.refreshConfig();
